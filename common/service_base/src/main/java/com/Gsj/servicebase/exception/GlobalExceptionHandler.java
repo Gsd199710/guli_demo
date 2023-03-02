362b1f5f595732ca0216ebfa61f300a14fbecd44
Gsd199710 <gsj1727450297@163.com>
2023-03-02 13:31:33 +0800

    add code

--- common/service_base/src/main/java/com/Gsj/servicebase/exception/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/Gsj/servicebase/exception/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.Gsj.servicebase.exception;
 
 
 import com.Gsj.utils.Results;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j//开启logback日志监控
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody//返回异常数据
@@ -18,6 +20,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(GuliException.class)
     @ResponseBody//返回异常数据
     public Results error(GuliException e){
+        log.error(e.getMessage());//logback自带的输出信息到日志文件中
         e.printStackTrace();
         return Results.error().code(e.getCode()).message(e.getMsg());
     }
